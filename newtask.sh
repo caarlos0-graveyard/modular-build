54959dac171d210127be687bcdce8e8125abc912
Carlos Alexandro Becker <caarlos0@gmail.com>
2013-06-10 10:32:07 -0300

    improvements on newtask.sh :innocent:

--- newtask.sh
+++ newtask.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 source $(dirname $0)/base.sh
 
+# not-so-pretty little hack for TAB char inside heredocs
+TAB="$(printf '\t')"
+
 header "Creating a new task..."
 
-echo "Please inform the new task name:"
-read name
+# prompts for new script name (with hack to do it in the same line)
+printf 'Please inform the new task name: '
+read -r name
+
+# create it and fix permissions
 cp ./scripts/{example,$name}.sh
 chmod +x ./scripts/$name.sh
 
+# append content to Makefile
 cat <<EOT >> Makefile
 
 # call ${name}
 ${name}:
-  \$(call script,${name})
+${TAB}\$(call script,${name})
 EOT
 
+# done
 section_done "${name} task created."
-
 footer
